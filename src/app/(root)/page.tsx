b8dd7bd99b8f6d62c0dc85ff563bcd3845fbf4f5
progwired <progwired@gmail.com>
2025-05-25 11:41:39 +0530

    package/shadcnui-added

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function HomePage() {
     return (
         <div>
             Hello from Home Page
+            <Button>Hello Click me</Button>
         </div>
     )
 }

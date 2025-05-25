e390852095059395862d8f3950f485a934b3e079
progwired <progwired@gmail.com>
2025-05-25 13:46:15 +0530

    structure/header-footer-add-testlink

--- src/components/shared/header/TopNav.tsx
+++ src/components/shared/header/TopNav.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function TopNav() {
     return (
         <nav className='flex gap-4'>
-            <Link href={"/about"}>About</Link>
+            <Link href={"/think"}>About</Link>
             <Link href={"/products"}>Products</Link>
         </nav>
     )

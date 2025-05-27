24ae6574e79d62efc0b58fb193b714060a25e762
progwired <progwired@gmail.com>
2025-05-27 13:01:47 +0530

    feature/zod-added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,6 +6,12 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 //convert prisma object into a regular JS object
-export function convertDbObjToJSObj<T>(value: T): T {
+export function utilDbObjToJSObj<T>(value: T): T {
   return JSON.parse(JSON.stringify(value))
+}
+
+//format number with decimal places
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".")
+  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`;
 }
